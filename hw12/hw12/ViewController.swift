1435260a47eaaf2294005277b28c35106a5064cc
alexey-z-m <alexey.panda@gmail.com>
2022-05-31 15:29:01 +0300

    correction of remarks

--- hw12/hw12/ViewController.swift
+++ hw12/hw12/ViewController.swift
@@ -1,179 +1,145 @@
-//
-//  ViewController.swift
-//  hw12
-//
-//  Created by Panda on 24.05.2022.
-//
-
 import UIKit
-
 class ViewController: UIViewController {
-    
     private var isWorkTime: Bool = true
     private var isStarted: Bool = false
     private var isPause: Bool = false
-    
-    
     private var timer = Timer()
+    private let timeToWork = 10.0
+    private let timeToRest = 5.0
     private var displayTimer = 10.0
     
-    
-    private lazy var lblType: UILabel = {
-        var lbl = UILabel()
-            
-        lbl.text = Strings.lblIsWorkTime
-        lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.font = .systemFont(ofSize: 40, weight: .bold)
-        lbl.textColor = Metric.green
-        return lbl
+    private lazy var labelType: UILabel = {
+        var label = UILabel()
+        label.text = Strings.labelIsWorkTime
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 40, weight: .bold)
+        label.textColor = Metric.green
+        return label
     }()
     
-    private lazy var lblReset: UIButton = {
-        var btn = UIButton(type: .system)
-            
-        btn.setTitle(Strings.btnReset, for: .normal)
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
-        btn.setTitleColor(Metric.reset, for: .normal)
-        btn.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
-        
-        return btn
+    private lazy var labelReset: UIButton = {
+        var button = UIButton(type: .system)
+        button.setTitle(Strings.buttonReset, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
+        button.setTitleColor(Metric.reset, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
     }()
     
     private lazy var stackView: UIStackView = {
         var stackView = UIStackView()
-            
         stackView.axis = .vertical
         stackView.spacing = 10
         stackView.distribution = .fillProportionally
-            
         return stackView
     }()
     
-    private lazy var btnTimer: UIButton = {
-        let btn = UIButton()
-        
-        btn.addTarget(self, action: #selector(runNpauseTimer), for: .touchUpInside)
-        
-        return btn
+    private lazy var buttonTimer: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(runNpauseTimer), for: .touchUpInside)
+        return button
     }()
-        private lazy var imgStartPause: UIImageView = {
-            var imgSP = UIImageView()
-            
-            let img = UIImage(systemName: "play.circle")
-            imgSP.image = img
-            imgSP.tintColor = Metric.green
-            
-            return imgSP
-        }()
     
-    private lazy var lblTimer: UILabel = {
-        var lbl = UILabel()
-            
-        lbl.text = convertToTimer(displayTimer: displayTimer) //"00:00"
-        lbl.font = .systemFont(ofSize: 40, weight: .bold)
-        lbl.textColor = Metric.green
-        return lbl
+    private lazy var imageStartPause: UIImageView = {
+        var imageSP = UIImageView()
+        let image = UIImage(systemName: "play.circle")
+        imageSP.image = image
+        imageSP.tintColor = Metric.green
+        return imageSP
+    }()
+    
+    private lazy var labelTimer: UILabel = {
+        var label = UILabel()
+        label.text = convertToTimer(displayTimer: displayTimer) //"00:00"
+        label.font = .systemFont(ofSize: 40, weight: .bold)
+        label.textColor = Metric.green
+        return label
     }()
     
     private lazy var circle: UIView = {
         let view = UIView()
-        
         view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)
         view.layer.cornerRadius = Metric.circleWidth / 2
         view.clipsToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
-        
         return view
     }()
     
-    
     let shapeLayer = CAShapeLayer()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       
         setupView()
         setupHierarchy()
         setupLayout()
-        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        animationCircle()
+        startCircleAnimation()
     }
     
     private func setupHierarchy() {
         view.addSubview(circle)
         circle.layer.addSublayer(shapeLayer)
-        view.addSubview(btnTimer)
-        view.addSubview(lblType)
-        view.addSubview(lblReset)
-        
-        //btnTimer.addSubview(circle)
-        btnTimer.addSubview(lblTimer)
-        btnTimer.addSubview(imgStartPause)
+        view.addSubview(buttonTimer)
+        view.addSubview(labelType)
+        view.addSubview(labelReset)
+        buttonTimer.addSubview(labelTimer)
+        buttonTimer.addSubview(imageStartPause)
     }
     
     private func setupLayout() {
-        
-        btnTimer.translatesAutoresizingMaskIntoConstraints = false
-        btnTimer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        btnTimer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        btnTimer.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        btnTimer.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        
-        //stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        lblType.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        lblType.bottomAnchor.constraint(equalTo: btnTimer.topAnchor, constant: -50).isActive = true
-        
+        buttonTimer.translatesAutoresizingMaskIntoConstraints = false
+        buttonTimer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonTimer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        buttonTimer.widthAnchor.constraint(equalToConstant: Metric.buttonTimerWidth).isActive = true
+        buttonTimer.heightAnchor.constraint(equalToConstant: Metric.buttonTimerHeight).isActive = true
+        labelType.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelType.bottomAnchor.constraint(equalTo: buttonTimer.topAnchor, constant: -50).isActive = true
         circle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         circle.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         circle.heightAnchor.constraint(equalToConstant: Metric.circleHeight).isActive = true
         circle.widthAnchor.constraint(equalToConstant: Metric.circleWidth).isActive = true
-        
-        lblTimer.translatesAutoresizingMaskIntoConstraints = false
-        lblTimer.centerXAnchor.constraint(equalTo: btnTimer.centerXAnchor).isActive = true
-        lblTimer.topAnchor.constraint(equalTo: btnTimer.topAnchor, constant: 50).isActive = true
-            
-        
-        imgStartPause.translatesAutoresizingMaskIntoConstraints = false
-        imgStartPause.centerXAnchor.constraint(equalTo: btnTimer.centerXAnchor).isActive = true
-        imgStartPause.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        imgStartPause.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-        imgStartPause.topAnchor.constraint(equalTo: lblTimer.bottomAnchor, constant: 30).isActive = true
-        
-        lblReset.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        lblReset.topAnchor.constraint(equalTo: btnTimer.bottomAnchor, constant: 20).isActive = true
-        lblReset.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        lblReset.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        
+        labelTimer.translatesAutoresizingMaskIntoConstraints = false
+        labelTimer.centerXAnchor.constraint(equalTo: buttonTimer.centerXAnchor).isActive = true
+        labelTimer.topAnchor.constraint(equalTo: buttonTimer.topAnchor, constant: 50).isActive = true
+        imageStartPause.translatesAutoresizingMaskIntoConstraints = false
+        imageStartPause.centerXAnchor.constraint(equalTo: buttonTimer.centerXAnchor).isActive = true
+        imageStartPause.widthAnchor.constraint(equalToConstant: Metric.imageStartPauseWidth).isActive = true
+        imageStartPause.heightAnchor.constraint(equalToConstant: Metric.imageStartPauseHeight).isActive = true
+        imageStartPause.topAnchor.constraint(equalTo: labelTimer.bottomAnchor, constant: 30).isActive = true
+        labelReset.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelReset.topAnchor.constraint(equalTo: buttonTimer.bottomAnchor, constant: 20).isActive = true
+        labelReset.widthAnchor.constraint(equalToConstant: Metric.labelResetWidth).isActive = true
+        labelReset.heightAnchor.constraint(equalToConstant: Metric.labelResetHeight).isActive = true
     }
     
     private func setupView() {
-        
-        //btnTimer.backgroundColor = #colorLiteral(red: 0.07412432879, green: 0.08454861492, blue: 0.2357147634, alpha: 0.3927750126)
         view.backgroundColor = Metric.darkGreen
     }
     
+    func pauseTimer() {
+        timer.invalidate()
+        pauseAnimation()
+        isPause = true
+        isStarted = false
+        imageStartPause.image = UIImage(systemName: "play.circle")
+    }
+    
     @objc func runNpauseTimer() {
-        
-        
         if isStarted {
-            timer.invalidate()
-            pauseAnimation()
-            isPause = true
-            isStarted = false
-            imgStartPause.image = UIImage(systemName: "play.circle")
+            pauseTimer()
         } else {
-            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(action), userInfo: nil, repeats: true)
-            imgStartPause.image = UIImage(systemName: "pause.circle.fill")
+            timer = Timer.scheduledTimer(
+                timeInterval: 0.01,
+                target: self,
+                selector: #selector(startTimer),
+                userInfo: nil,
+                repeats: true
+            )
+            imageStartPause.image = UIImage(systemName: "pause.circle.fill")
             isStarted = true
             if isPause {
                 resumeAnimation()
@@ -181,44 +147,53 @@ class ViewController: UIViewController {
             } else {
                 basicAnimation()
             }
-            
         }
     }
     
-    @objc func action() {
+    @objc func startTimer() {
         displayTimer -= 0.01
-        
-        if isWorkTime && displayTimer < 0 {
-            isWorkTime = false
-            displayTimer = 5
-            view.backgroundColor = Metric.darkBurgundy
-            lblTimer.textColor = Metric.burgundy
-            imgStartPause.tintColor = Metric.burgundy
-            lblType.text = Strings.lblIsNotWorkTime
-            lblType.textColor = Metric.burgundy
+        if isWorkTime && displayTimer < 1.2 {
             shapeLayer.strokeColor = Metric.burgundy.cgColor
-            
-            basicAnimation()
         }
-        if !isWorkTime && displayTimer < 0 {
-            isWorkTime = true
-            displayTimer = 10
-            view.backgroundColor = Metric.darkGreen
-            lblTimer.textColor = Metric.green
-            imgStartPause.tintColor = Metric.green
-            lblType.text = Strings.lblIsWorkTime
-            lblType.textColor = Metric.green
+        if !isWorkTime && displayTimer < 1.2 {
             shapeLayer.strokeColor = Metric.green.cgColor
-            
-            basicAnimation()
         }
-        
-        lblTimer.text = convertToTimer(displayTimer: displayTimer)
-        
+        if isWorkTime && displayTimer < 1 {
+            isWorkTime = false
+            displayTimer = timeToRest
+            changeColor(isWorkTime)
+            labelType.text = Strings.labelIsNotWorkTime
+            basicAnimation()
+            pauseTimer()
+        }
+        if !isWorkTime && displayTimer < 1 {
+            isWorkTime = true
+            displayTimer = timeToWork
+            changeColor(isWorkTime)
+            labelType.text = Strings.labelIsWorkTime
+            basicAnimation()
+            pauseTimer()
+        }
+        labelTimer.text = convertToTimer(displayTimer: displayTimer)
+    }
+    
+    func changeColor(_ isWorkTime: Bool) {
+        var mainColor: UIColor
+        var otherColor: UIColor
+        if isWorkTime {
+            mainColor = Metric.darkGreen
+            otherColor = Metric.green
+        } else {
+            mainColor = Metric.darkBurgundy
+            otherColor = Metric.burgundy
+        }
+        view.backgroundColor = mainColor
+        labelTimer.textColor = otherColor
+        imageStartPause.tintColor = otherColor
+        labelType.textColor = otherColor
     }
     
     func convertToTimer(displayTimer: Double) -> String {
-        
         var result = ""
         let min = Int(displayTimer) / 60
         let sec = Int(displayTimer) % 60
@@ -226,17 +201,20 @@ class ViewController: UIViewController {
         return result
     }
     
-    func animationCircle(){
-        
+    func startCircleAnimation(){
         let center = CGPoint(x: circle.frame.width / 2, y: circle.frame.height / 2)
-        
         let endAngle = (-CGFloat.pi / 2)
         let startAngle = 2 * CGFloat.pi + endAngle
-        let circlePath = UIBezierPath(arcCenter: center, radius: Metric.circleWidth / 2 - 5, startAngle: startAngle, endAngle: endAngle, clockwise: false)
-        
+        let circlePath = UIBezierPath(
+            arcCenter: center,
+            radius: Metric.circleWidth / 2 - 5,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: false
+        )
         shapeLayer.path = circlePath.cgPath
         shapeLayer.lineWidth = 10
-        shapeLayer.fillColor = #colorLiteral(red: 0.2180668712, green: 0.03185554966, blue: 0.1179199442, alpha: 0)
+        shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.strokeEnd = 1
         shapeLayer.lineCap = .round
         if isWorkTime {
@@ -248,18 +226,16 @@ class ViewController: UIViewController {
     
     func basicAnimation() {
         let animation = CABasicAnimation(keyPath: "strokeEnd")
-        
         animation.toValue = 0
-        animation.duration = CFTimeInterval(displayTimer)
+        animation.duration = CFTimeInterval(displayTimer-1)
         animation.fillMode = .forwards
-        animation.isRemovedOnCompletion = true
         shapeLayer.speed = 1
         shapeLayer.add(animation, forKey: "basicAnimation")
     }
     
     func pauseAnimation() {
         let pausedTime = shapeLayer.convertTime(CACurrentMediaTime(), from: nil)
-        shapeLayer.speed = 0.0
+        shapeLayer.speed = 0
         shapeLayer.timeOffset = pausedTime
     }
     
@@ -273,49 +249,47 @@ class ViewController: UIViewController {
     }
     
     @objc func resetTimer() {
-        
-        //shapeLayer.strokeColor = enabled ? Metric.green.cgColor : Metric.burgundy.cgColor
         shapeLayer.strokeColor = Metric.green.cgColor
-        
         isWorkTime = true
         shapeLayer.speed = 0.0
         shapeLayer.timeOffset = 0
         isStarted = false
         isPause = false
-        imgStartPause.image = UIImage(systemName: "play.circle")
+        imageStartPause.image = UIImage(systemName: "play.circle")
         displayTimer = 10
-        lblTimer.text = convertToTimer(displayTimer: displayTimer)
+        labelTimer.text = convertToTimer(displayTimer: displayTimer)
         view.backgroundColor = Metric.darkGreen
-        lblTimer.textColor = Metric.green
-        imgStartPause.tintColor = Metric.green
-        lblType.text = "time to work"
-        lblType.textColor = Metric.green
+        labelTimer.textColor = Metric.green
+        imageStartPause.tintColor = Metric.green
+        labelType.text = "time to work"
+        labelType.textColor = Metric.green
         timer.invalidate()
         shapeLayer.speed = 0.1
         shapeLayer.strokeColor = Metric.green.cgColor
-        
     }
-
 }
-
 
 extension ViewController {
     enum Metric {
+        static let buttonTimerHeight: CGFloat = 200
+        static let buttonTimerWidth: CGFloat = 200
+        static let imageStartPauseHeight: CGFloat = 40
+        static let imageStartPauseWidth: CGFloat = 40
         static let circleHeight: CGFloat = 200
         static let circleWidth: CGFloat = 200
-        
+        static let labelResetHeight: CGFloat = 100
+        static let labelResetWidth: CGFloat = 200
         static let green = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
         static let darkGreen = #colorLiteral(red: 0.1676505506, green: 0.2282019258, blue: 0.1698732376, alpha: 1)
         static let burgundy = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
         static let darkBurgundy = #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1)
-        
         static let reset = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
     }
     
     enum Strings {
-        static let lblIsWorkTime: String = "time to work"
-        static let lblIsNotWorkTime: String = "time to rest"
-        static let btnReset: String = "Reset timer"
+        static let labelIsWorkTime: String = "time to work"
+        static let labelIsNotWorkTime: String = "time to rest"
+        static let buttonReset: String = "Reset timer"
     }
 }
 
